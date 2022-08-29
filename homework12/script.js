b0e4805d74a0a34afe58320c418f1525ae33e9a0
Arapov Vasyl <panda.dagon@gmail.com>
2022-08-29 14:02:16 +0300

    additional tasks

--- homework12/script.js
+++ homework12/script.js
@@ -1,12 +1,28 @@
 const someString = 'This is some strange string';
 
 function reverse(str) {
-
+    let output = '';
+    if (typeof (str) !== 'string') {
+        output = 'Ошибка!';
+    } else {
+        output = str.split('').reverse().join('');
+    }
+    return output;
 }
 
 const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
 function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function (curr) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`
+        }
+    });
+    return str;
 
 }
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
